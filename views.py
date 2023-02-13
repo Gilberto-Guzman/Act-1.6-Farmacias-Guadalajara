41cab3b6fdad82841789ec204cc70b7a380f14e5
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-02-13 00:01:27 -0600

    Se agrego el modulo de login y register

--- views.py
+++ views.py
@@ -11,3 +11,13 @@ def home():
 @views.route("/contact")
 def contact():
     return render_template("views/contact/contact.html")
+
+
+@views.route("/login")
+def login():
+    return render_template("views/login/login.html")
+
+
+@views.route("/register")
+def register():
+    return render_template("views/register/register.html")
