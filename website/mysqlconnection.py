584fa18333cc2fd42388ff2cfa3f0e07768f49f2
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2023-05-11 14:03:07 -0600

    testeando funcionalidades

--- website/mysqlconnection.py
+++ website/mysqlconnection.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 
 app.secret_key = '1ec8fb36c578c721e00d4143c6d2c2f66bbffb4d7fdb58bcf36a6a8deac713bc'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'CHICHARITOBLUE4'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'CHICHARITOBLUE4'
+# app.config['MYSQL_DB'] = 'farmaciasguadalajara'
+
+app.config['MYSQL_HOST'] = 'db-mysql-nyc1-73231-do-user-14016281-0.b.db.ondigitalocean.com'
+app.config['MYSQL_USER'] = 'doadmin'
+app.config['MYSQL_PASSWORD'] = 'AVNS_4pFIZDPxXNQmGZE1Axx'
 app.config['MYSQL_DB'] = 'farmaciasguadalajara'
+app.config['MYSQL_PORT'] = 25060
+app.config['MYSQL_SSL_MODE'] = 'REQUIRED'
+
 mysql = MySQL(app)
